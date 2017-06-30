f15acaf4b1378045637f7f1ed541c966d3b3c6aa
jwats287 <jwats287@users.noreply.github.com>
2017-06-30 09:09:48 -0400

    Created main.js file and folder

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var dealerCardOne;
+var dealerCardTwo;
+var dealerCardThree;
+var dealerCardFour;
+var dealerCardFive;
+var dealerCardSix;
+var dealerCardSeven;
+var playerCardOne;
+var playerCardTwo;
+var playerCardThree;
+var playerCardFour;
+var playerCardFive;
+var playerCardSix;
+var playerCardSeven;
+
+var dealerCards[];
+var playerCards[];
+var usedCards[];
+
+var dealerScore;
+var playerScore;
+
+playerScore = 
+
+//
