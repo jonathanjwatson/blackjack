2d3ada53913cfcee964ffe55051043bbf490813c
jwats287 <jwats287@users.noreply.github.com>
2017-07-12 10:26:38 -0400

    Added pseudocode file

---
+++ js/pseudocode.js
@@ -0,0 +1,61 @@
+//create a function to generate a random number, equal to cards in deck. 
+
+//Create an array for Deck of Cards. 
+//Include name, value and eventually, image source. 
+
+//Create a function that looks through usedCards array and make sure the number isn't in there. If it is, loop back and find a different number card. 
+
+//Create INitial Setup Function
+
+// Get one random card and assign to dealer. Hide this card.
+// Get one random card and assign to player. Display this card. 
+// Get one random card and assign to dealer. Display this card. 
+// Get one random card and assign it to player. Display this card. 
+
+
+//Push dealer cards into dealer array. 
+//Return dealer score
+
+//Calculate Dealer Score function. Loop through array and add up values. 
+
+//Advanced feature. If dealer score = 21, display Blackjack.
+
+//Push player cards into player array. 
+//Return player Score
+
+//Calculate Player Score function. Loop through array and add up values. 
+
+//Create a Stay Function for player. 
+//IF player hits Stay, push playerCards into usedCards array. 
+// Create "Hit" function for player. When player selects "hit" get a new random card and add to player array. Display the card.
+
+//If player points exceeds 21, alert "Bust!" and push playerCards into usedCards array. ;
+
+//Once Player is done taking cards, show dealer cards, then start taking dealer cards. 
+
+//Create Function to Start dealing dealer cards.
+//Include IF statement for Dealer Logic. If Dealer Score = 17, stop.
+
+//When everybody has as many cards as they want, evaluate who has a higher score. 
+
+//Advanced: Create Split Button for player. 
+//If playerCards[0].value === playerCards[1].value, then create Split Button. 
+
+
+//Advanced: Create Double Button for player. 
+
+
+
+
+
+//Create Click Handler for Deal Button
+//Should run Initial Setup Function
+
+//Create Click Handler for Hit Button
+//Should run Hit function
+
+//Create Click Handler for Stay Button
+//Should run Stay Function
+
+
+
